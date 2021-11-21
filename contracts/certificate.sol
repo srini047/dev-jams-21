973e1da15c2c556f17518e93ff5407232bf0964c
Sriniketh J <srinikethcr7@gmail.com>
2021-11-21 19:20:32 +0530

    prettier run on smart contract

--- contracts/certificate.sol
+++ contracts/certificate.sol
@@ -2,37 +2,38 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract certi {
-    
-  address admin;
-  
-  constructor() {
-    admin = msg.sender;    
-  }
-  
-  modifier onlyAdmin {
-      require(msg.sender == admin, "Insuficient privilage");
-      _ ;
-  }
-  
-  struct certificate {
-      string courseName;
-      string candidateName;
-      string grade;
-      string date;
-  }
-  
-  mapping (string => certificate) public certificateDetails;
-  
-  function newCertificate (
-      string memory _certificateID,
-      string memory _courseName,
-      string memory _candidateName,
-      string memory _grade,
-      string memory _date ) public onlyAdmin {
-          certificateDetails[_certificateID] = certificate(
-                                                    _courseName,
-                                                    _candidateName,
-                                                    _grade,
-                                                    _date);
-      }
+    address admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Insuficient privilage");
+        _;
+    }
+
+    struct certificate {
+        string courseName;
+        string candidateName;
+        string grade;
+        string date;
+    }
+
+    mapping(string => certificate) public certificateDetails;
+
+    function newCertificate(
+        string memory _certificateID,
+        string memory _courseName,
+        string memory _candidateName,
+        string memory _grade,
+        string memory _date
+    ) public onlyAdmin {
+        certificateDetails[_certificateID] = certificate(
+            _courseName,
+            _candidateName,
+            _grade,
+            _date
+        );
+    }
 }
